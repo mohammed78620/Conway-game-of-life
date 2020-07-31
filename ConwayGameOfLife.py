5d92f18218a10d5506120635eb7cd47a30b832ab
mohammedmiah67890@gmail.com <mohammedmiah67890@gmail.com>
2020-07-31 13:38:09 +0100

    able to choose hieght and width

--- ConwayGameOfLife.py
+++ ConwayGameOfLife.py
@@ -1,11 +1,19 @@
 import keyboard
-from copy import copy, deepcopy
 import pygame
 import Constants
 import sys
 import numpy as np
 from random import randint
 import time 
+
+def copy(T):
+    tTemp = np.zeros((Constants.NUMBER_OF_BLOCKS_HIGH, Constants.NUMBER_OF_BLOCKS_WIDE), dtype=np.int)
+    for i in range(len(T)):
+        for j in range(len(T[i])):
+            tTemp[i][j] = T[i][j]
+    return tTemp
+
+
 
 def get_tile_colour(no):
     tile_color = Constants.YELLOW
@@ -85,6 +93,14 @@ def check_dead_cell(T, i, j):
 #     [0,0,0,0,0,0,0,0,0,1,0,0],
 #     [0,0,0,0,0,0,0,0,0,0,0,0]]
 
+
+#set the grid size
+rows = int(input("Enter how many rows: "))
+cols = int(input("Enter how many columns: "))
+
+Constants.NUMBER_OF_BLOCKS_HIGH = rows
+Constants.NUMBER_OF_BLOCKS_WIDE = cols
+
 T = np.zeros((Constants.NUMBER_OF_BLOCKS_HIGH, Constants.NUMBER_OF_BLOCKS_WIDE), dtype=np.int)
 # T = np.random.randint(0,2, size=(Constants.NUMBER_OF_BLOCKS_HIGH,Constants.NUMBER_OF_BLOCKS_WIDE))
 
@@ -94,59 +110,60 @@ T = np.zeros((Constants.NUMBER_OF_BLOCKS_HIGH, Constants.NUMBER_OF_BLOCKS_WIDE),
 # T[6][5] = 1
 # T[7][6] = 1
 
-T[25][25] = 1
+# T[25][25] = 1
+# T[25][26] = 1
+# T[25][27] = 1
+# T[26][25] = 1
+# T[27][26] = 1
+
 T[25][26] = 1
-T[25][27] = 1
 T[26][25] = 1
-T[27][26] = 1
+T[27][25] = 1
+T[28][25] = 1
+T[28][26] = 1
+T[28][27] = 1
+T[28][28] = 1
+T[27][29] = 1
+T[25][29] = 1
 
 
 
+for i in range(500):
+    T[randint(0, len(T)-1)][randint(0, len(T[0])-1)] = 1
 
-# for i in range(185):
-#     T[randint(0, len(T)-1)][randint(0, len(T[0])-1)] = 1
-
-
-SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 400
-YELLOW = (200, 200, 20)
 
 pygame.init()
-surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WIDTH))
-pygame.display.set_caption("hello world")
+surface = pygame.display.set_mode((Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT))
+pygame.display.set_caption("conway game of life")
 surface.fill(Constants.YELLOW)
 
-
+clock = pygame.time.Clock()
+tempT = copy(T)
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.K_e:
-            pygame.quit()
-            sys.exit()
-        draw_map(surface, T)
-        pygame.display.update()
-        tempT = deepcopy(T)
-        # when space pressed next move
-        # keyboard.wait('space')
-        # every move in 0.5 secs
-        time.sleep(0.25)
-        print_matrix(tempT)
-        #check every cell
-        for i in range(len(T)):
-            for j in range(len(T[i])):
-                if T[i][j] == 1:
-                    if check_alive_cell(T, i, j):
-                        tempT[i][j] = 0
-                        print()
-                    # print(i,j)
-                else:
-                    # print(check_dead_cell(tempT, i, j))
-                    if check_dead_cell(T, i, j): 
-                        tempT[i][j] = 1
-        # print_matrix(tempT)
-        T = deepcopy(tempT)
+    pygame.event.pump()
+    if keyboard.is_pressed('q'):
+        pygame.quit()
+        sys.exit()
+    draw_map(surface, T)
+    pygame.display.update()
+    
+    clock.tick(600)
+    print_matrix(tempT)
+    #check every cell
+    for i in range(len(T)):
+        for j in range(len(T[i])):
+            if T[i][j] == 1:
+                if check_alive_cell(T, i, j):
+                    tempT[i][j] = 0
+                    print()
+                # print(i,j)
+            else:
+                # print(check_dead_cell(tempT, i, j))
+                if check_dead_cell(T, i, j): 
+                    tempT[i][j] = 1
+    # print_matrix(tempT)
+    # T = deepcopy(tempT)
+    T = copy(tempT)
 
         
 
