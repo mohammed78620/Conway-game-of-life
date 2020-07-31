7c6d55f58f1e0cc18ea6d888338598785ae3547c
mohammedmiah67890@gmail.com <mohammedmiah67890@gmail.com>
2020-07-31 20:32:11 +0100

    code clean up

--- ConwayGameOfLife.py
+++ ConwayGameOfLife.py
@@ -46,17 +46,10 @@ def check_alive_cell(T, i, j):
 
     for x in range(rmin, rmax + 1):
         for y in range(cmin, cmax + 1):
-            # print(x, " ", y , " --->", T[x][y])
             if ((i == x) and (j == y)):
                 count += 0
             elif T[x][y] == 1:
                 count += 1    
-            
-    
-                
-    # print()
-    print("count is ==",count<2,"i,j == ",i," ",j)
-    # print()
     return count<2 or count>3
 
 
@@ -76,96 +69,49 @@ def check_dead_cell(T, i, j):
                 count += 0
             if T[x][y] == 1:
                 count += 1
-    # print("count is ==",count>2,"i,j == ",i," ",j)
-    
     return ((count>2) and (count<4))
-
-# T = [[0,0,0,0,0,0,0,0,0,0,0,0],
-#     [0,0,0,0,0,0,0,0,0,0,0,0],
-#     [0,0,0,0,0,0,0,0,0,0,0,0],
-#     [0,0,0,0,0,0,0,0,0,0,0,0],
-#     [0,0,0,0,0,0,0,0,0,0,0,0],
-#     [0,0,0,0,0,0,0,0,0,0,0,0],
-#     [0,0,0,0,0,1,0,0,0,1,0,0],
-#     [0,0,0,0,0,1,0,0,0,0,0,0],
-#     [0,0,0,0,0,1,0,0,0,0,0,0],
-#     [0,0,0,0,0,0,0,0,0,0,0,0],
-#     [0,0,0,0,0,0,0,0,0,1,0,0],
-#     [0,0,0,0,0,0,0,0,0,0,0,0]]
-
-
-#set the grid size
-rows = int(input("Enter how many rows: "))
-cols = int(input("Enter how many columns: "))
-
-Constants.NUMBER_OF_BLOCKS_HIGH = rows
-Constants.NUMBER_OF_BLOCKS_WIDE = cols
-
-T = np.zeros((Constants.NUMBER_OF_BLOCKS_HIGH, Constants.NUMBER_OF_BLOCKS_WIDE), dtype=np.int)
-# T = np.random.randint(0,2, size=(Constants.NUMBER_OF_BLOCKS_HIGH,Constants.NUMBER_OF_BLOCKS_WIDE))
-
-# T[5][5] = 1
-# T[5][6] = 1
-# T[5][7] = 1
-# T[6][5] = 1
-# T[7][6] = 1
-
-# T[25][25] = 1
-# T[25][26] = 1
-# T[25][27] = 1
-# T[26][25] = 1
-# T[27][26] = 1
-
-T[25][26] = 1
-T[26][25] = 1
-T[27][25] = 1
-T[28][25] = 1
-T[28][26] = 1
-T[28][27] = 1
-T[28][28] = 1
-T[27][29] = 1
-T[25][29] = 1
-
-
-
-for i in range(500):
-    T[randint(0, len(T)-1)][randint(0, len(T[0])-1)] = 1
-
-
-pygame.init()
-surface = pygame.display.set_mode((Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT))
-pygame.display.set_caption("conway game of life")
-surface.fill(Constants.YELLOW)
-
-clock = pygame.time.Clock()
-tempT = copy(T)
-while True:
-    pygame.event.pump()
-    if keyboard.is_pressed('q'):
-        pygame.quit()
-        sys.exit()
-    draw_map(surface, T)
-    pygame.display.update()
     
-    clock.tick(600)
-    print_matrix(tempT)
-    #check every cell
-    for i in range(len(T)):
-        for j in range(len(T[i])):
-            if T[i][j] == 1:
-                if check_alive_cell(T, i, j):
-                    tempT[i][j] = 0
-                    print()
-                # print(i,j)
-            else:
-                # print(check_dead_cell(tempT, i, j))
-                if check_dead_cell(T, i, j): 
-                    tempT[i][j] = 1
-    # print_matrix(tempT)
-    # T = deepcopy(tempT)
-    T = copy(tempT)
+if __name__ == "__main__":
+    #set the grid size
+    rows = int(input("Enter how many rows: "))
+    cols = int(input("Enter how many columns: "))
 
-        
+    Constants.NUMBER_OF_BLOCKS_HIGH = rows
+    Constants.NUMBER_OF_BLOCKS_WIDE = cols
+
+    Constants.BLOCK_HEIGHT = round(Constants.SCREEN_HEIGHT/Constants.NUMBER_OF_BLOCKS_HIGH)
+    Constants.BLOCK_WIDTH = round(Constants.SCREEN_WIDTH/Constants.NUMBER_OF_BLOCKS_WIDE)
+
+    T = np.zeros((Constants.NUMBER_OF_BLOCKS_HIGH, Constants.NUMBER_OF_BLOCKS_WIDE), dtype=np.int)
+
+    for i in range(int((Constants.NUMBER_OF_BLOCKS_HIGH*Constants.NUMBER_OF_BLOCKS_WIDE)/3)):
+        T[randint(0, len(T)-1)][randint(0, len(T[0])-1)] = 1
+
+
+    pygame.init()
+    surface = pygame.display.set_mode((Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT))
+    pygame.display.set_caption("conway game of life")
+    surface.fill(Constants.YELLOW)
+    tempT = copy(T)
+    while True:
+        pygame.event.pump()
+        if keyboard.is_pressed('q'):
+            pygame.quit()
+            sys.exit()
+        draw_map(surface, T)
+        pygame.display.update()
+        print_matrix(tempT)
+        #check every cell
+        for i in range(len(T)):
+            for j in range(len(T[i])):
+                if T[i][j] == 1:
+                    if check_alive_cell(T, i, j):
+                        tempT[i][j] = 0
+                else:
+                    if check_dead_cell(T, i, j): 
+                        tempT[i][j] = 1
+        T = copy(tempT)
+ 
 
 
 
